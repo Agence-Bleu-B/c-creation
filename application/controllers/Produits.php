92968576fa050dd6d299743d45964d1818c68372
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-08-30 17:24:38 +0200

    ajout "index" aux produits

--- application/controllers/Produits.php
+++ application/controllers/Produits.php
@@ -2,6 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Produits extends CI_Controller {
+	public function index()
+	{
+		$datahead = array();
+		$datahead['title'] = 'c-creation/catalogue';
+		$datahead['description'] = 'c-creation';
+		$data = array();
+		$this->load->view('common/header',$datahead);
+        $this->load->view('produits/catalogue',$data);
+        $this->load->view('common/footer');
+	}
 	public function lampes()
 	{
 		$datahead = array();
