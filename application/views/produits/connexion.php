e73d812e4bfc4459b0e7f625335ea2f46ccfae5d
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-09-05 12:28:47 +0200

    mise en route "connection"

--- application/views/produits/connexion.php
+++ application/views/produits/connexion.php
@@ -6,15 +6,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
   <section class="home_elmt row">
        <article class="col-md-6" id="formulaire_co">
-         <form >
+         <form method="post" action="">
             
             <h5>Login</h5>
-            <input class="input_form" type="text" name="login" value="">
+            <input class="input_form" type="text" name="login">
             <h5>Mot De Passe</h5>
-            <input class="input_form" type="password" name="login" value="">
+            <input class="input_form" type="password" name="mdp">
 
             <div id="sub_co">
-                <input type="submit" value="Connexion">
+                <input type="submit" value="Connexion" name="conect">
             </div>
              
         </form>
