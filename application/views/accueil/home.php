10b8e37e356de7c643ec6774c9e66c68ef4b89d7
SullivanAnge <ange.sullivan@hotmail.fr>
2017-10-26 15:02:34 +0200

    affichage nouveauté

--- application/views/accueil/home.php
+++ application/views/accueil/home.php
@@ -51,9 +51,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <article class="col-md-9">
             <h2 class="title_nouv">Nouveautés</h2>
              <!-- Produits -->
-         <!--   <ul class="row">
+            <ul class="row">
                 
                 <li class="col-md-4 product_item">
+                    <h2>lampes</h2>
                     <a href="#">
                         <p>Ref. 0071</p>
                         <img class="img-responsive" src="http://cdn.maisonsdumonde.com/img/lampe-de-bureau-en-metal-rose-h-38-cm-pix-500-9-33-150544_0.jpg">
@@ -61,6 +62,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </li>
                 
                 <li class="col-md-4 product_item">
+                <h2>lampes</h2>
                     <a href="#">
                         <p>Ref. 0071</p>
                         <img class="img-responsive" src="http://cdn.maisonsdumonde.com/img/suspension-blanche-en-metal-d-50-cm-finmark-500-6-9-138097_1.jpg">
@@ -68,6 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </li>
                
                 <li class="col-md-4 product_item">
+                <h2>lampes</h2>
                     <a href="#">
                         <p>Ref. 0070</p>
                         <img class="img-responsive" src="http://cdn.maisonsdumonde.com/img/applique-etirable-en-metal-grise-h-13-cm-lorient-500-6-0-148297_1.jpg">
@@ -75,6 +78,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </li>
                 
                 <li class="col-md-4 product_item">
+                <h2>lampes</h2>
                     <a href="#">
                         <p>Ref. 0070</p>
                         <img class="img-responsive" src="http://www.maisonsdumonde.com/img/lampe-en-metal-noir-h-41-cm-ikat-500-3-14-158766_1.jpg">
@@ -82,6 +86,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </li>
                 
                 <li class="col-md-4 product_item">
+                <h2>lampes</h2>
                     <a href="#">
                         <p>Ref. 0070</p>
                         <img class="img-responsive" src="http://cdn.maisonsdumonde.com/img/lampadaire-trepied-en-metal-cuivre-h-174-cm-conquerant-500-8-17-130602_1.jpg">
@@ -89,15 +94,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </li>
                 
                 <li class="col-md-4 product_item">
+                <h2>lampes</h2>
                     <a href="#">
                         <p>Ref. 0070</p>
                         <img class="img-responsive" src="http://cdn.maisonsdumonde.com/img/suspension-non-electrifiee-en-metal-bleu-clair-d-33cm-confluent-500-14-4-167940_1.jpg">
                     </a>
                 </li>
             </ul> 
-          -->
           
-          <h3 class="col-md-offset-4"> Indisponible pour le moment </h3>  
+          
+          
         </article>
     </section>
 
