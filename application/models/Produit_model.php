7d5237842038ef85e8e148c787e2763427226a82
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-10-26 17:34:23 +0200

    ajout model produit

---
+++ application/models/Produit_model.php
@@ -0,0 +1,8 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Produit_model extends CI_Model
+{
+    //récupération de tous les produits
+    public function get_all(){
+
+    }
+}
