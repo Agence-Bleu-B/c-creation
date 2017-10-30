83787bd334f6bf465b6e37e90888a812378ed429
SullivanAnge <ange.sullivan@hotmail.fr>
2017-10-30 15:33:39 +0100

    ajout table nouveauté

---
+++ nouveaute.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Lun 30 Octobre 2017 à 14:33
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `c-creation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nouveaute`
+--
+
+CREATE TABLE `nouveaute` (
+  `ref` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `nouveaute`
+--
+
+INSERT INTO `nouveaute` (`ref`) VALUES
+(307903),
+(309146),
+(306478),
+(820655),
+(304900),
+(226460),
+(223987),
+(309759),
+(308144);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
