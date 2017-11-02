2b06fa1cbf9739e1a91b6f251a049e94fc20a410
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-11-02 17:58:27 +0100

    up du soir

--- application/views/admin/clients.php
+++ application/views/admin/clients.php
@@ -18,17 +18,47 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <!-- debut bloc new textes -->
             <div id="new" class="col-md-12 jumbotron lity-hide" style="margin-left: 30px;margin-right: 100px;width: 80%;border-radius: 10px;">
               <form method="post" action="<?php echo site_url('admin/realisations'); ?>">
-                <div class="col-md-12" >Titre</div>
-                <div class="col-md-12" ><input type="text" name="titre" style="width: 100%;margin-bottom: 15px;" ></div>
-                <div class="col-md-12" >Texte</div>
-                <div class="col-md-12" ><textarea name="texte" style="width: 100%;height: 250px;margin-bottom: 15px;" ></textarea></div>
-                <div class="col-md-12" ><input type="submit" class="btn btn-default" name="new" value="Créer"></div>
+                <div class="col-md-12" >login</div>
+                <div class="col-md-12" >
+                    <input type="text" name="login" style="width: 100%;margin-bottom: 15px;" >
+                </div>
+                <div class="col-md-12" >mot de passe</div>
+                <div class="col-md-12" >
+                    <input type="text" name="mdp" style="width: 100%;margin-bottom: 15px;" >
+                </div>
+                <div class="col-md-12" >
+                    <input type="submit" class="btn btn-default" name="new" value="Créer">
+                </div>
               </form>
             </div>
+            <div class="col-md-12" >
+            <!-- fin bloc new textes -->
             <?php 
-            foreach ($clients_liste as $key => $value) {
-              print_r($value); echo '<br>';
-            } ?>
+            foreach ($clients_liste as $key => $value) { ?>
+                <div class="col-md-1" >
+                    <a href="<?php echo '#'.$value['id'] ;?>" data-lity >
+                        <div class="btn btn-default" ><?php echo $value['login']; ?></div>
+                    </a>
+                </div>
+            <?php } ?>
+            </div>
+                  <!-- <div id="<?php echo $value['id'] ;?>" class="col-md-12 jumbotron lity-hide" style="margin-left: 30px;margin-right: 100px;width: 80%;border-radius: 10px;">
+                  <form method="post" action="<?php echo site_url('admin/realisations'); ?>">
+                    <div class="col-md-12" >login</div>
+                    <div class="col-md-12" >
+                        <input type="text" name="login" style="width: 100%;margin-bottom: 15px;" >
+                    </div>
+                    <div class="col-md-12" >mot de passe</div>
+                    <div class="col-md-12" >
+                        <input type="text" name="mdp" style="width: 100%;margin-bottom: 15px;" >
+                    </div>
+                    <div class="col-md-12" >
+                        <input type="submit" class="btn btn-default" name="modif" value="Modifier">
+                    </div>
+                  </form>
+                </div> -->
+              
+            
         </div>
         <!-- /. contenu  -->
          <!-- /. ROW  -->           
