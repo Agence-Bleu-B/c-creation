a375a24501ae4c64d6c59182b1f097c212461ccf
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-11-20 12:27:20 +0100

    avancée nouvetés

--- application/views/admin/nouveautes.php
+++ application/views/admin/nouveautes.php
@@ -12,15 +12,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       	<hr />
       	<!-- contenu a partir d'ici -->
       	<div class="row text-center pad-top">
-          	<?php for ($i=0; $i < count($list_nouv); $i++) { ?>
-                    
-                    
-                    
-                        
-                        <p class="col-md-4 product_item"><?php echo $list_nouv[$i]['nom'];?></p>
-                    
-              
-               <?php }?>
+        <?php
+        foreach ($list_nouv as $key => $value) { ?>
+          <div class="col-md-4 product_item">
+            <?php echo $value['nom'];?>
+          </div>
+        <?php }?>
       	</div>
       	<!-- /. contenu  -->
          <!-- /. ROW  -->           
