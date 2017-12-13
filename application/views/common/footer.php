98fe10ff4ecbdee44040ebae5a8dab68af4f917a
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-12-13 13:49:00 +0100

    mod footer

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -5,14 +5,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div class="container">
 		<div class="row">
 			<p class="col-xs-12">							
-						<a href="#" title="Mentions légales">Mentions légales</a>
+						<!-- <a href="#" title="Mentions légales">Mentions légales</a>
 						&nbsp;|&nbsp;
 														
 						<a href="#" title="Plan du site">Plan du site</a>
 						&nbsp;|&nbsp;
 														
 						<a href="#" title="PACIFLORE PRUNE">Produit</a>
-						&nbsp;|&nbsp;
+						&nbsp;|&nbsp; -->
 													Copyright &copy;  2017 SAS Boudet  Tous droits réservés.
 				| <a href="http://www.bleu-b.com/" class="agence">Création <img src="<?php echo img_url('/logos/logo_footer_web.jpg'); ?>"></a>
 			</p>
